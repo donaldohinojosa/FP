f0abecad8247a5bd1737e4e407d27e16417a793d
Your Name <you@example.com>
2021-09-17 18:31:16 -0500

    EXAMEN 1

--- EXAMEN 1.py
+++ EXAMEN 1.py
@@ -1,4 +1,4 @@
-print("EXAMÉN UNIDAD 1")
+"EXAMÉN UNIDAD 1"
 
 
    
@@ -7,7 +7,7 @@ Nombre del alumno=
 Lugar de residencia=
 "El Milagro"
 Fecha de Nacimiento=
-"Aguascaleientes, Mexico"
+"22 de Mayo del 2003"
 Color favorito=
 "Verde"
 Animal Favorito=
@@ -15,14 +15,14 @@ Animal Favorito=
 
 
 ¿Qué es la programación?
-"proceso que involucra el diseño, codificación, mantenimiento, y protección de cualquier fuente de programas."
+"es el proceso que involucra el diseño, codificación, mantenimiento, y protección de cualquier fuente de programas."
 ¿Por qué es importante la programación?
-"organiza el proceso de idear y ordenar las acciones necesarias para que un determinado proyecto pueda ejecutarse en las computadoras."
+"porque organiza el proceso de idear y ordenar las acciones necesarias para que un determinado proyecto pueda ejecutarse en las computadoras."
 Explique con sus palabras que es herencia.
 "es un mecanismo que permite la definición de una clase a partir de la definición de otra ya existente."
 Explique con sus palabras que es poliformismo.
-"la propiedad por la que es posible enviar mensajes sintácticamente iguales a objetos de tipos distintos."
+"es la propiedad por la que es posible enviar mensajes sintácticamente iguales a objetos de tipos distintos."
 Explique con sus palabras que es abstracción.
-"La abstracción consiste en aislar un elemento de su contexto o del resto de los elementos que lo acompañan."
+"la abstracción consiste en aislar un elemento de su contexto o del resto de los elementos que lo acompañan."
 Explique con sus palabras que es encapsulamiento.
-"ocultamiento del estado, es decir, de los datos miembro de un objeto de manera que solo se pueda cambiar mediante las operaciones definidas para ese objeto."
+"el ocultamiento del estado, es decir, de los datos miembro de un objeto de manera que solo se pueda cambiar mediante las operaciones definidas para ese objeto."
