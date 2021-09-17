3ac0c1f857370f78c3f0b9cd44f14df3341534a9
Your Name <you@example.com>
2021-09-17 18:22:46 -0500

    EXAMEN 1

--- EXAMEN 1.py
+++ EXAMEN 1.py
@@ -1,23 +1,28 @@
 print("EXAMÉN UNIDAD 1")
 
-<!DOCTYPE html>
-<html lang="es">
-   
-   </head>
-   <meta charset="utf-8" />
-   </head>
 
-   <body>
-   "Nombre del alumno.
-Lugar de residencia
-Fecha de Nacimiento
-Color favorito
-Animal Favorito"
+   
+Nombre del alumno=
+"Donaldo Ramses Hinojosa Meza"
+Lugar de residencia=
+"El Milagro"
+Fecha de Nacimiento=
+"Aguascaleientes, Mexico"
+Color favorito=
+"Verde"
+Animal Favorito=
+"Aguila real"
 
 
 ¿Qué es la programación?
+"proceso que involucra el diseño, codificación, mantenimiento, y protección de cualquier fuente de programas."
 ¿Por qué es importante la programación?
+"organiza el proceso de idear y ordenar las acciones necesarias para que un determinado proyecto pueda ejecutarse en las computadoras."
 Explique con sus palabras que es herencia.
+"es un mecanismo que permite la definición de una clase a partir de la definición de otra ya existente."
 Explique con sus palabras que es poliformismo.
+"la propiedad por la que es posible enviar mensajes sintácticamente iguales a objetos de tipos distintos."
 Explique con sus palabras que es abstracción.
+"La abstracción consiste en aislar un elemento de su contexto o del resto de los elementos que lo acompañan."
 Explique con sus palabras que es encapsulamiento.
+"ocultamiento del estado, es decir, de los datos miembro de un objeto de manera que solo se pueda cambiar mediante las operaciones definidas para ese objeto."
