630780c9d30da78441f054aafa3b8f400e8ad0e0
Your Name <you@example.com>
2021-09-17 17:42:49 -0500

    Examen

---
+++ EXAMEN 1.py
@@ -0,0 +1,23 @@
+print("EXAMÉN UNIDAD 1")
+
+<!DOCTYPE html>
+<html lang="es">
+   
+   </head>
+   <meta charset="utf-8" />
+   </head>
+
+   <body>
+   "Nombre del alumno.
+Lugar de residencia
+Fecha de Nacimiento
+Color favorito
+Animal Favorito"
+
+
+¿Qué es la programación?
+¿Por qué es importante la programación?
+Explique con sus palabras que es herencia.
+Explique con sus palabras que es poliformismo.
+Explique con sus palabras que es abstracción.
+Explique con sus palabras que es encapsulamiento.
