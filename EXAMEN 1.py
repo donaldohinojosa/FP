5c192786ae6498c11ee275a7de3f40ca1b629025
Your Name <you@example.com>
2021-09-17 22:58:07 -0500

    EXAMEN 1

--- EXAMEN 1.py
+++ EXAMEN 1.py
@@ -1,28 +1,45 @@
-"EXAMÉN UNIDAD 1"
+print("EXAMÉN UNIDAD 1")
 
 
-   
-Nombre del alumno=
+ 
+print("Datos del alumno")
+
+
+print("Nombre del alumno")=
 "Donaldo Ramses Hinojosa Meza"
-Lugar de residencia=
+
+print("Lugar de residencia")=
 "El Milagro"
-Fecha de Nacimiento=
+
+print("Fecha de Nacimiento")=
 "22 de Mayo del 2003"
-Color favorito=
+
+print("Color favorito")=
 "Verde"
-Animal Favorito=
+
+print("Animal Favorito")=
 "Aguila real"
 
 
-¿Qué es la programación?
+
+ 
+print("Contesta las siguientes preguntas")
+
+
+1.print("¿Qué es la programación?") 
 "es el proceso que involucra el diseño, codificación, mantenimiento, y protección de cualquier fuente de programas."
-¿Por qué es importante la programación?
+
+2.print("¿Por qué es importante la programación?")  
 "porque organiza el proceso de idear y ordenar las acciones necesarias para que un determinado proyecto pueda ejecutarse en las computadoras."
-Explique con sus palabras que es herencia.
+
+3.print("Explique con sus palabras que es herencia.")
 "es un mecanismo que permite la definición de una clase a partir de la definición de otra ya existente."
-Explique con sus palabras que es poliformismo.
+
+4.print("Explique con sus palabras que es poliformismo.")
 "es la propiedad por la que es posible enviar mensajes sintácticamente iguales a objetos de tipos distintos."
-Explique con sus palabras que es abstracción.
+
+5.print("Explique con sus palabras que es abstracción.")
 "la abstracción consiste en aislar un elemento de su contexto o del resto de los elementos que lo acompañan."
-Explique con sus palabras que es encapsulamiento.
+
+6.print("Explique con sus palabras que es encapsulamiento.")
 "el ocultamiento del estado, es decir, de los datos miembro de un objeto de manera que solo se pueda cambiar mediante las operaciones definidas para ese objeto."
