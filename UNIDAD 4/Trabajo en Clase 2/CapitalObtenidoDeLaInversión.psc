54d25f8fe376503d5b82e8ff0d84a732d4a002e3
Your Name <you@example.com>
2021-11-29 18:18:35 -0600

    .

---
+++ UNIDAD 4/Trabajo en Clase 2/CapitalObtenidoDeLaInversión.psc
@@ -0,0 +1,14 @@
+Proceso CapitalObtenidoDeLaInversión
+	Escribir "Escribe la cantidad de dinero a invertir: ";
+	Leer cantidad;
+	gananciatotal <- cantidad*1 + cantidad; 
+//	
+	Escribir "Escribe el interes anual"
+	Leer interes
+	
+	Escribir "Escribe el numero de años: ";
+	Leer años;
+	 
+	
+	Escribir "Total despues de un año: ",gananciatotal;
+FinProceso
---
+++ UNIDAD 4/Trabajo en Clase 2/SueldoEnBaseAHorasTrabajadas.py
@@ -0,0 +1,33 @@
+n = int(input ("Ingresa un numero:"))
+
+if n == 1:
+   print("Tu sueldo es de $18")
+elif n == 2:
+   print("Tu sueldo es de $36")
+elif n == 3:
+   print("Tu sueldo es de $54")
+elif n == 4:
+   print("Tu sueldo es de $72")
+elif n == 5:
+   print("Tu sueldo es de $90")
+elif n == 6:
+   print("Tu sueldo es de $108")
+elif n == 7:
+   print("Tu sueldo es de $126")
+elif n == 8:
+   print("Tu sueldo es de $144")
+elif n == 9:
+   print("Tu sueldo es de $162")
+elif n == 10:
+   print("Tu sueldo es de $180")
+elif n == 11:
+   print("Tu sueldo es de $198")
+elif n == 12:
+   print("Tu sueldo es de $216")
+elif n == 13:
+   print("Tu sueldo es de $234")
+elif n == 14:
+   print("Tu sueldo es de $252")
+elif n == 15:
+   print("Tu sueldo es de $270")
+
