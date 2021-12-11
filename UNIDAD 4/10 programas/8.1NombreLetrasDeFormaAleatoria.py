3a6e70da96ac6672cad0e5e120ae767b7e29399d
Your Name <you@example.com>
2021-12-10 20:15:32 -0600

    .

---
+++ UNIDAD 4/10 programas/8.1NombreLetrasDeFormaAleatoria.py
@@ -0,0 +1,9 @@
+nombre = input (" Introduce tu nombre: ")
+print ("¡Hola " + nombre + "!")
+
+>>> import string
+>>> string.ascii_letters
+'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+>>> import random
+>>> random.choice(string.ascii_letters)
+'j'
